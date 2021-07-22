4a56847a3f209d93f674d5f929994dcce39bad99
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-22 16:25:13 -0700

    in progress

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var $car = document.querySelector('.car');
 
 document.addEventListener('keydown', handleKey);
-
+document.addEventListener('keydown', handleStart);
 function handleKey(event) {
   if (event.key === 'ArrowDown') {
     $car.className = 'car rotate-down';
@@ -10,6 +10,38 @@ function handleKey(event) {
   } else if (event.key === 'ArrowUp') {
     $car.className = 'car rotate-up';
   } else {
-    $car.className = 'car rotate-right)';
+    $car.className = 'car rotate-right';
+  }
+}
+
+// function handleStart(event) {
+//   var posLeft = 0;
+//   setInterval(frame, 5);
+
+//   function frame() {
+//     if (event.key === ' ') {
+//       posLeft++;
+//     }
+//     if (posLeft === 1300) {
+//       while (posLeft > 0) {
+//         posLeft--;
+//       }
+//     }
+//     $car.style.left = posLeft + 'px';
+//   }
+// }
+
+function handleStart(event) {
+  $car.style.left = '0px';
+  setInterval(moveRight, 16);
+  function moveRight() {
+    if (event.key === ' ') {
+      $car.style.left = parseInt($car.style.left) + 15 + 'px';
+    }
+    if ($car.style.left === '1300px') {
+      while ($car.style.left > 0) {
+        $car.style.left--;
+      }
+    }
   }
 }
